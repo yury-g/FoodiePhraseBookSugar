c7cc4004c5bf63874d043d780c548540d46a3611
Voltaire's Ghost <yury@pulsesensor.com>
2016-04-19 11:20:12 -0400

    Initial Commit

---
+++ FoodiePhraseBookSugar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FoodiePhraseBookSugar
+//
+//  Created by Yoda Dev on 4/19/16.
+//  Copyright © 2016 Yury G. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
